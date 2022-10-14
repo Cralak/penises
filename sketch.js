d50dbf8ae373cbda6ef1efeb37a62dd33167d16b
Cralak <115760654+Cralak@users.noreply.github.com>
2022-10-14 18:36:30 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ cockTimer = 0.0;
 cockScale = 1;
 var background;
 var multipleCocks;
-var cockRotation;
+cockRotation = 0;
 
 function setup() {
   width = screen.height;
@@ -14,6 +14,7 @@ function setup() {
   background = createGraphics(width, height);
   background.background(0);
   noCursor();
+  rectMode(CENTER);
 }
 
 
@@ -23,7 +24,7 @@ function draw() {
     cockScale += cockTimer
   }  
   image(background, 0, 0)
-  image(multipleCocks, 0, 0, )
+  image(multipleCocks, 0, 0)
   cursorPenisDraw(mouseX,mouseY,cockScale);
 }
 
@@ -32,7 +33,7 @@ function penisDraw(x,y,scale) {
   multipleCocks.stroke(0);
   multipleCocks.strokeWeight(scale);
   multipleCocks.fill(random(0,255),random(0,255),random(0,255));
-  multipleCocks.arc(x, y - 15*scale, 10*scale, 10*scale, PI, 2 * PI);
+  multipleCocks.circle(x, y - 15*scale, 10*scale);
   multipleCocks.beginShape();
   multipleCocks.vertex(x + 5*scale, y + 10*scale);
   multipleCocks.vertex(x + 5*scale, y - 15*scale);
@@ -52,7 +53,7 @@ function mousePressed() {
 
 function mouseReleased() {
   cockTimerIsOn = false;
-  penisDraw(mouseX,mouseY,cockScale);
+  newPenisDraw(mouseX,mouseY,cockScale);
   cockScale = 1;
   cockTimer = 0;
 }
@@ -61,16 +62,53 @@ function cursorPenisDraw(x,y,scale) {
   stroke(255);
   strokeWeight(scale);
   noFill();
-  arc(x, y - 15*scale, 10*scale, 10*scale, PI, 2 * PI);
-  beginShape();
-  vertex(x + 5*scale, y + 5*scale);
-  vertex(x + 5*scale, y - 15*scale);
-  vertex(x, y - 15*scale);
-  vertex(x, y - 20*scale);
-  vertex(x, y - 15*scale);
-  vertex(x - 5*scale,  y - 15*scale);
-  vertex(x - 5*scale, y + 5*scale);
-  endShape();
-  circle(x + 5*scale, y + 10*scale, 10*scale);
-  circle(x - 5*scale, y + 10*scale, 10*scale);
+  translate(x, y)
+  rotate(radians(-cockRotation))
+  translate(0, -15 * scale)
+  arc(0, 0, 10*scale, 10*scale, PI, 2 * PI);
+  translate(0, +15 * scale)
+  beginShape()
+  vertex(5 * scale, 5 * scale)
+  vertex(5 * scale, -15*scale)
+  vertex(0, - 15 * scale)
+  vertex(0, - 20 * scale)
+  vertex(0, - 15 * scale)
+  vertex(-5 * scale, - 15 * scale)
+  vertex(-5 * scale, + 5 * scale)
+  endShape()
+  circle(5 * scale, 10 * scale, 10 * scale)
+  circle(-5 * scale, 10 * scale, 10 * scale)
+}
+
+function newPenisDraw(x,y,scale) {
+  multipleCocks.stroke(0);
+  multipleCocks.strokeWeight(scale);
+  multipleCocks.fill(random(0,255),random(0,255),random(0,255));
+  multipleCocks.translate(x,y)
+  multipleCocks.rotate(radians(-cockRotation))
+  multipleCocks.translate(0, -15 * scale)
+  multipleCocks.arc(0, 0, 10*scale, 10*scale, PI, 2 * PI);
+  multipleCocks.translate(0, +15 * scale)
+  multipleCocks.beginShape()
+  multipleCocks.vertex(5 * scale, 10 * scale)
+  multipleCocks.vertex(5 * scale, -15*scale)
+  multipleCocks.vertex(0, - 15 * scale)
+  multipleCocks.vertex(0, - 20 * scale)
+  multipleCocks.vertex(0, - 15 * scale)
+  multipleCocks.vertex(-5 * scale, - 15 * scale)
+  multipleCocks.vertex(-5 * scale, + 10 * scale)
+  multipleCocks.endShape()
+  multipleCocks.circle(5 * scale, 10 * scale, 10 * scale)
+  multipleCocks.circle(-5 * scale, 10 * scale, 10 * scale)
+  multipleCocks.rotate(radians(cockRotation))
+  multipleCocks.translate(-x,-y)
+}
+
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    cockRotation += 10
+  }
+  else {
+    cockRotation -= 10
+  }
 }
